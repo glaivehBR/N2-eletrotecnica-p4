7fac0b79a6122d099fa2070e652b58bb83b5e251
Glaive Helles <125269667+glaivehBR@users.noreply.github.com>
2023-05-11 09:20:37 -0300

    Update Q5.c

--- vetores/Q5.c
+++ vetores/Q5.c
@@ -4,18 +4,18 @@ int main() {
     int vetor[8];
     int num, i, posicao = -1;
 
-    // Preenchendo o vetor com valores fornecidos pelo usuário
+    
     printf("Digite 8 números inteiros:\n");
     for (i = 0; i < 8; i++) {
         printf("Número %d: ", i + 1);
         scanf("%d", &vetor[i]);
     }
 
-    // Solicitando um número do teclado
+    
     printf("Digite um número: ");
     scanf("%d", &num);
 
-    // Pesquisando se o número existe no vetor
+    
     for (i = 0; i < 8; i++) {
         if (vetor[i] == num) {
             posicao = i;
@@ -23,7 +23,7 @@ int main() {
         }
     }
 
-    // Exibindo o resultado da pesquisa
+    
     if (posicao != -1) {
         printf("O número %d está na posição %d do vetor.\n", num, posicao);
     } else {
